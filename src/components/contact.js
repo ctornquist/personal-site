5199879f2c459713a7835399643e026571adb5d7
caroline tornquist <caroline.22@dartmouth.edu>
2020-10-22 11:52:44 -0400

    form validation

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,6 @@
 import '../style.scss';
 import React, { Component } from 'react';
+// import axios from 'axios';
 
 class Contact extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ class Contact extends Component {
       email: '',
       name: '',
       message: '',
+      err: false,
     };
   }
 
@@ -30,10 +32,10 @@ class Contact extends Component {
         <hr />
         <form id="contact-form" method="post" onSubmit="return validateForm()" action="https://formspree.io/caroline.22@dartmouth.edu">
           <div className="form-group">
-            <input onChange={this.changeName} value={this.state.name} placeholder="name" />
+            <input onChange={this.changeName} value={this.state.name} placeholder="name" required />
           </div>
           <div className="form-group">
-            <input onChange={this.changeEmail} value={this.state.email} placeholder="email" />
+            <input type="email" onChange={this.changeEmail} value={this.state.email} placeholder="email" required />
           </div>
           <div className="form-group">
             <textarea id="message" onChange={this.changeMessage} value={this.state.message} placeholder="message" />
@@ -41,6 +43,7 @@ class Contact extends Component {
           <div className="form-group">
             <button type="submit" className="btn btn-primary">Send</button>
           </div>
+          { this.state.err ? (<p> Please fill out all form fields</p>) : (<p />) }
         </form>
       </div>
     );
