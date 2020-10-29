d53dfb38af983b21fba8b227defc907d6974a55e
caroline tornquist <caroline.22@dartmouth.edu>
2020-10-29 17:55:43 -0400

    kinda did spinner

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,10 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../style.scss';
-import React from 'react';
+import React, { Component } from 'react';
 import 'react-slideshow-image/dist/styles.css';
 import { Fade } from 'react-slideshow-image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loader-spinner';
 import Baker from '../images/baker.jpeg';
 import Fall from '../images/fall.jpeg';
 import Franconia from '../images/franconia.jpeg';
@@ -21,60 +22,80 @@ const fadeProperties = {
   arrows: false,
 };
 
-const Welcome = (props) => {
-  return (
-    <div className="welcome" id="welcome">
-      <div className="name">
-        <h1>CAROLINE TORNQUIST</h1>
-        <h2>dartmouth &#39;22 computer science major</h2>
-        <div className="socials">
-          <a href="https://github.com/ctornquist" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} className="fa-2x" />
-          </a>
-          <a href="https://www.linkedin.com/in/carolinetornquist/" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} className="fa-2x" />
-          </a>
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  changeLoaded = () => {
+    console.log('calling change load');
+    this.setState({ isLoading: false });
+  }
+
+  render() {
+    console.log('loaded', this.state.isLoading);
+    const val = this.state.isLoading ? 'none' : 'flex';
+    return (
+      <div>
+        <Loader type="Puff" timeout="3000" color="#3aafa9" />
+        <div className="welcome" id="welcome" style={{ display: { val } }}>
+          <div className="name">
+            <h1>CAROLINE TORNQUIST</h1>
+            <h2>dartmouth &#39;22 computer science major</h2>
+            <div className="socials">
+              <a href="https://github.com/ctornquist" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faGithub} className="fa-2x" />
+              </a>
+              <a href="https://www.linkedin.com/in/carolinetornquist/" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} className="fa-2x" />
+              </a>
+            </div>
+          </div>
+          <Fade {...fadeProperties}>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={Morey} alt="first" onLoad={this.changeLoaded} />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={Sunrise} alt="first" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={Franconia} alt="first" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={Mink} alt="first" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={Baker} alt="first" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={Normandy} alt="first" />
+              </div>
+            </div>
+            <div className="each-fade">
+              <div className="image-container">
+                <img src={Fall} alt="first" />
+              </div>
+            </div>
+          </Fade>
         </div>
       </div>
-      <Fade {...fadeProperties}>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={Morey} alt="first" />
-          </div>
-        </div>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={Sunrise} alt="first" />
-          </div>
-        </div>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={Franconia} alt="first" />
-          </div>
-        </div>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={Mink} alt="first" />
-          </div>
-        </div>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={Baker} alt="first" />
-          </div>
-        </div>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={Normandy} alt="first" />
-          </div>
-        </div>
-        <div className="each-fade">
-          <div className="image-container">
-            <img src={Fall} alt="first" />
-          </div>
-        </div>
-      </Fade>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Welcome;
